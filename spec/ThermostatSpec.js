941b7d95c0bb90adf536f9a4ab43c707d043217a
matteomanzo <imatteo.manzo@gmail.com>
2015-01-20 16:55:49 +0000

    thermostat can decrease temperature, test fails

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,29 @@
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  describe('by default', function() {
+
+    it('starts at 20 degrees', function() {
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('changing temperature', function() {
+
+    it('can increase', function() {
+      thermostat.increaseTemperature(1);
+      expect(thermostat.temperature).toEqual(21);
+    });
+
+    it('can decrease', function() {
+      thermostat.decreaseTemperature(1);
+      expect(thermostat.temperature).toEqual(19);
+    });
+
+  });
+});
