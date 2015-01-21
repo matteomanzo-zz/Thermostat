dae93f7310b73f6e4d3bb70685a264f3f58d4ffa
Jack Rubio <jackrubio26@gmail.com>
2015-01-21 17:15:52 +0000

    butons work and discovered a few back end bugs

--- js/interface.js
+++ js/interface.js
@@ -10,4 +10,19 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature);
   });
 
+  $('.decrease-temp').on('click', function() {
+    thermostat.decreaseTemperature(1);
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('.reset').on('click', function() {
+    thermostat.reset();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('.PSM-toggle').on('click', function() {
+    thermostat.powerSavingToggle();
+    $('#temperature').text(thermostat.temperature);
+  });
+
 });
