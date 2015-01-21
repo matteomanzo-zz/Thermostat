a2044096949da2886670b5c5c27d51f995262cbd
matteomanzo <imatteo.manzo@gmail.com>
2015-01-21 18:44:15 +0000

    changing colors on interface

--- js/interface.js
+++ js/interface.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
   $('.PSM-toggle').on('click', function() {
     thermostat.powerSavingToggle();
     updateTemperature();
+    $(this).css('color','red');
   });
 
 });
